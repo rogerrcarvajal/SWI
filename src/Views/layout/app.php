fcbaa55b6081ed040395f0a444105cd6bfa751b3
rogerrcarvajal <roger.carvajal@gmail.com>
2025-09-19 22:13:05 -0400

    SWI - Act varias

--- src/Views/layout/app.php
+++ src/Views/layout/app.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sistema de Inventario</title>
+    <title>SWI - Sistema de Inventario</title>
     <base target="_top">
     <link rel="stylesheet" href="css/style.css">
 </head>
